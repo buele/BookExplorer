b0408e104feeb28577b4f61264664ede893a4ecc
Buele <info@buele.me>
2014-03-31 00:21:59 +0200

    entry poing

---
+++ FreebaseBookSpider/BookSpider.m
@@ -0,0 +1,93 @@
+//
+//  BookSpider.m
+//  Freebase interface object to look topics about books
+//
+//  Created by Raffaele Bua on 28/03/14.
+
+/*****************************************************************************
+ The MIT License (MIT)
+ 
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ *****************************************************************************/
+
+
+#import "BookSpider.h"
+
+@implementation BookSpider
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        freebaseUrls = [self getFreebaseBaseUrls];
+        mqlQueries = [self getMQLQueries];
+        NSString * allTypesOfBookDomainUrl = [self getAllTypesOfBookDomainUrl];
+        NSLog(@"url: %@", [self encodeUrl:allTypesOfBookDomainUrl]);
+    }
+    
+    return self;
+}
+
+-(NSString * )getAllTypesOfBookDomainUrl
+{
+    NSString * allTypesOfBookDomainQuery = [mqlQueries objectForKey:@"get_book_domain_all_types"];
+    
+    return [NSString stringWithFormat:@"%@?query=%@",[self getRunMqlQueryUrl],allTypesOfBookDomainQuery];
+}
+
+-(NSString* )encodeUrl:(NSString*)url
+{
+    return [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+}
+
+-(NSString* )getRunMqlQueryUrl
+{
+    return [NSString stringWithFormat:@"%@%@",
+           [self getBaseUrl],
+           [freebaseUrls objectForKey:@"run_query_url"]];
+}
+
+-(NSDictionary* )getFreebaseBaseUrls
+{
+    NSString* path =  [[NSBundle mainBundle] pathForResource:@"freebase_urls" ofType:@"plist"];
+    return [[NSDictionary alloc] initWithContentsOfFile:path];
+}
+
+
+-(NSString*) getBaseUrl
+{
+    return [freebaseUrls objectForKey:@"api_base_url"];
+}
+
+
+-(NSDictionary* )getMQLQueries
+{
+    NSString* path =  [[NSBundle mainBundle] pathForResource:@"mql_queries" ofType:@"plist"];
+    
+    return [[NSDictionary alloc] initWithContentsOfFile:path];
+}
+
+-(NSArray *)getBookDomainTypes
+{
+    return nil;
+    
+}
+
+@end
---
+++ FreebaseBookSpiderTests/FreebaseBookSpiderTests.m
@@ -0,0 +1,34 @@
+//
+//  FreebaseBookSpiderTests.m
+//  FreebaseBookSpiderTests
+//
+//  Created by Raffaele Bua on 28/03/14.
+//  Copyright (c) 2014 Buele. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FreebaseBookSpiderTests : XCTestCase
+
+@end
+
+@implementation FreebaseBookSpiderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
